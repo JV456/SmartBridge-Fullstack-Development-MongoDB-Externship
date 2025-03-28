02023696fee80c57df79db134cfdc6e87a9743d7
Jai Vadula <jaivadula@gmail.com>
2025-03-28 16:49:35 +0530

    Project Initials Setup and defining app routes

---
+++ PersonalExpenseTrackerApp/frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Expense from './pages/Dashboard/Expense';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/SignUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
+
+const Root = () => {
+  // Check if the token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
