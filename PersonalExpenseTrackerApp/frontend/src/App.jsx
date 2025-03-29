ca917a6e35fb5c86bb31fb5c99a981eb7d91172d
Jai Vadula <jaivadula@gmail.com>
2025-03-29 22:30:03 +0530

    Frontend setup

--- PersonalExpenseTrackerApp/frontend/src/App.jsx
+++ PersonalExpenseTrackerApp/frontend/src/App.jsx
@@ -1,44 +1,9 @@
-import React from 'react';
-import {
-  Navigate,
-  Route,
-  BrowserRouter as Router,
-  Routes
-} from "react-router-dom";
-import Login from './pages/Auth/Login';
-import SignUp from './pages/Auth/SignUp';
-import Expense from './pages/Dashboard/Expense';
-import Home from './pages/Dashboard/Home';
-import Income from './pages/Dashboard/Income';
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/SignUp" exact element={<SignUp />} />
-          <Route path="/dashboard" exact element={<Home />} />
-          <Route path="/income" exact element={<Income />} />
-          <Route path="/expense" exact element={<Expense />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <h1 class="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    </>
   )
 }
 
-export default App;
-
-const Root = () => {
-  // Check if the token exists in localStorage
-  const isAuthenticated = !!localStorage.getItem("token");
-
-  // Redirect to dashboard if authenticated, otherwise to login
-
-  return isAuthenticated ? (
-    <Navigate to="/dashboard" />
-  ) : (
-    <Navigate to="/login" />
-  );
-};
+export default App
